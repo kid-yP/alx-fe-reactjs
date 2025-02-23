9e102e7a7f48655c03f0d75a2adb255fbf08562b
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2025-02-23 22:18:49 +0300

    Update MainContent.jsx

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
 
-function MainContent(props) {
-  return (
-    <main style={{ padding: '20px', margin: '20px auto', backgroundColor: '#f0f8ff', borderRadius: '8px', width: '80%', textAlign: 'justify' }}>
-      {props.children}
+function MainContent(){
+  return(
+    <main>
       <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
+
   );
 }
+
 
 export default MainContent;
