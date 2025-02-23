59fb1292c744fabb605d7ddcfdba43f5cc1a9822
Kidus <kidusmekuria11@gmail.com>
2025-02-23 22:12:09 +0300

    Add a css style to each of the JSx files

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const MainContent = () => {
+    return (
+      <main style={{ padding: '20px', backgroundColor: '#f0f0f0' }}>
+        <h2 style={{ color: 'darkgreen' }}>Welcome to My Favorite Cities</h2>
+        <p style={{ color: 'darkgreen' }}>Here you can find information about various cities.</p>
+      </main>
+    );
+  };
+  export default MainContent;
