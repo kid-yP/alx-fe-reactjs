3e035b38370446049d073a11b58108632847ce69
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2025-02-23 22:15:23 +0300

    Update MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -5,7 +5,7 @@ const MainContent = () => {
     return (
       <main style={{ padding: '20px', backgroundColor: '#f0f0f0' }}>
         <h2 style={{ color: 'darkgreen' }}>Welcome to My Favorite Cities</h2>
-        <p style={{ color: 'darkgreen' }}>Here you can find information about various cities.</p>
+        <p>Here you can find information about various cities.</p>
       </main>
     );
   };
