37b1609ac2802e9610fef98f7d02f10b854f5e85
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2025-02-24 00:55:08 +0300

    Delete my-company directory

--- my-company/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-      <footer style={{ padding: '10px', textAlign: 'center', backgroundColor: '#f8f9fa' }}>
-        <p>&copy; 2023 My Company. All rights reserved.</p>
-      </footer>
-    );
-  }
-  
-  export default Footer;
