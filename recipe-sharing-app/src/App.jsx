fc46b9db643f69f2852384774eec7e0695277147
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2025-03-04 14:36:15 +0300

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,8 +1,9 @@
+// src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
-import RecipeDetailsWrapper from './components/RecipeDetailsWrapper';
+import RecipeDetails from './components/RecipeDetails';
 import SearchBar from './components/SearchBar';
 import FavoritesList from './components/FavoritesList';
 import RecommendationsList from './components/RecommendationsList';
@@ -11,14 +12,15 @@ const App = () => {
   return (
     <Router>
       <div>
+        <h1>Recipe Sharing App</h1>
         <SearchBar />
+        <AddRecipeForm />
         <Routes>
           <Route path="/" element={<RecipeList />} />
-          <Route path="/add" element={<AddRecipeForm />} />
-          <Route path="/recipe/:recipeId" element={<RecipeDetailsWrapper />} />
-          <Route path="/favorites" element={<FavoritesList />} />
-          <Route path="/recommendations" element={<RecommendationsList />} />
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
         </Routes>
+        <FavoritesList /> {/* Ensure this is included */}
+        <RecommendationsList /> {/* Ensure this is included */}
       </div>
     </Router>
   );
