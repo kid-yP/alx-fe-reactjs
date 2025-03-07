d659dbfa93b26a14fa9b52d754c0fcf8b7e6d9a4
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2025-03-08 00:57:10 +0300

    Update App.jsx

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,13 +1,18 @@
 // src/App.jsx
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import PostsComponent from './PostsComponent';
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <div>
-      <h1>React Query Demo</h1>
-      <PostsComponent />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <h1>React Query Demo</h1>
+        <PostsComponent />
+      </div>
+    </QueryClientProvider>
   );
 };
 
