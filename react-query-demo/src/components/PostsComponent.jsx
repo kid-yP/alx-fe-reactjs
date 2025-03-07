b9761a957ed095f4af8ec0670c4ac96c96b01c7d
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2025-03-08 01:00:04 +0300

    Update PostsComponent.jsx

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,4 +1,4 @@
-// src/PostsComponent.jsx
+// src/components/PostsComponent.jsx
 import React from 'react';
 import { useQuery } from 'react-query';
 
@@ -11,26 +11,22 @@ const fetchPosts = async () => {
 };
 
 const PostsComponent = () => {
-  const { data, error, isLoading, refetch } = useQuery('posts', fetchPosts);
+  const { data, isError, isLoading } = useQuery('posts', fetchPosts);
 
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
+  if (isError) {
+    return <div>Error fetching posts</div>;
   }
 
   return (
     <div>
-      <h1>Posts</h1>
-      <button onClick={refetch}>Refetch Posts</button>
+      <h2>Posts</h2>
       <ul>
-        {data.map((post) => (
-          <li key={post.id}>
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
-          </li>
+        {data.map(post => (
+          <li key={post.id}>{post.title}</li>
         ))}
       </ul>
     </div>
