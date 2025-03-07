8b5207cb3c2f924c2f6f0ff6e4af143b68fceca9
Kidus <kidusmekuria11@gmail.com>
2025-03-08 00:55:21 +0300

    Initialize React project with Vite, add essential files and configurations

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import PostsComponent from './PostsComponent';
+
+const App = () => {
+  return (
+    <div>
+      <h1>React Query Demo</h1>
+      <PostsComponent />
+    </div>
+  );
+};
+
+export default App;
